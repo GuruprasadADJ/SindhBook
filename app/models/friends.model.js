a7524b2d1479ba73602b8bd5fdbad275ad1a4858
Guruprasad <guruprasad@adjinfotech.com>
2019-11-18 16:35:43 +0530

    added friends request and acceptance api

---
+++ app/models/friends.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const NoteSchema = mongoose.Schema({
+    //user_id
+    from_id: String,
+    to_id: String,
+    status:Number,
+    date:{type:Date, default:Date.Now}
+  });
+module.exports = mongoose.model('friend', NoteSchema);
