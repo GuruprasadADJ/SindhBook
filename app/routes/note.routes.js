18549af5fafd8d845268899be3cf1cec6e5d7e8a
Guruprasad <guruprasad@adjinfotech.com>
2019-11-19 15:49:11 +0530

    commits

--- app/routes/note.routes.js
+++ app/routes/note.routes.js
@@ -33,7 +33,7 @@ module.exports = (app) => {
 
     //post contents
     app.post('/post_content',post_content.content_create);
-    //post content update
+    ////post content update
     app.post('/post_content_update',post_content.post_content_update);
    
 }
