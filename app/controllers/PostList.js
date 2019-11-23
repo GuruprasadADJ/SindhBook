f332a9ce32afee87450744e826c5e77e0d469688
Guruprasad <guruprasad@adjinfotech.com>
2019-11-23 10:43:40 +0530

    changes in postlist

--- app/controllers/PostList.js
+++ app/controllers/PostList.js
@@ -7,78 +7,66 @@ exports.postList = (req, res) => {
     Note.findById(req.params.postId)
     .then(note=>{
         if(!note){
-            return res.status(200).send({result:'failed',
+            return res.status(200).send({result:'Failed',
                 message: "Data not found in database with this id " + req.params.postId
             });
         }
         else
         {
-            Post.find({          //checks wether the user_id exist in privacy table
+            var name=note.first_name+" "+note.last_name;
+            Post.find({         
                 "user_id": input
             }).then(post=>{
                 if(!post.length==0)
                 {
                     var ids=[];
-                    var user_ids=[];
                     for(var i=0;i<post.length; i++)
                     {
                         ids.push(post[i].id)
-                        user_ids.push(post[i].user_id);
                     }
-                     console.log("all ids  : ",ids);
-                     one(ids,user_ids);
-                     
+                    console.log("all ids  : ",ids);
+                    console.log("ids length="+ids.length);
+                    
+                    for(var i=0;i<ids.length;i++)
+                    {
+                        Post.find({
+                            "_id": ids[i]
+                        })
+                        .then(result=>{
+                            var json={};
+                               console.log("result.length ::",result.length);
+                               console.log(result[0]);
+                                json["title"]=result[0].title;
+                                json["contents"]=result[0].contents;
+                                json["likes"]=result[0].like;
+                                json["comments"]=result[0].comment;
+                                json["created_at"]=result[0].created_at;
+                                json["name"]=name;
+                                arraylist.push(json);
+                                if(arraylist.length==ids.length)
+                                {
+                                    console.log("arraylist:",arraylist);
+                                    res.send({result:"Success",message:"Data found successfully",data:arraylist});
+                                }
+                        }).catch(err => {
+                            res.status(500).send({
+                                result:"Failed",message:"There was an exception",errorMessage: err.message || "Some error occurred while creating the Note."
+                            });
+                        })
+                    }
                 }
                 else
                 {
-                    return res.status(200).send({result:'failed',message: "No posts found"});
+                    return res.status(200).send({result:'Failed',message: "No posts found"});
                 }
             }).catch(err => {
                 res.status(500).send({
-                    result:"failed",message:"There was an exception",errorMessage: err.message || "Some error occurred while creating the Note."
+                    result:"Failed",message:"There was an exception",errorMessage: err.message || "Some error occurred while creating the Note."
                 });
             })
         }
     }).catch(err => {
-        return res.status(200).send({result:'failed',
+        return res.status(200).send({result:'Failed',
                 message: "Data not found in database with this id " + req.params.postId});
     })
-
-function one(ids,user_ids)
-{ 
-    console.log("ids length="+ids.length);
-    var first_name=[],last_name=[];
-    
-    for(var j=0;j<user_ids.length;j++)
-    {
-        Note.find({
-            "_id": user_ids[j]
-        }).then(result1=>{
-            first_name.push(result1[0].first_name);
-            last_name.push(result1[0].last_name);
-           // name.push(result1[0].first_name+""+result1[0].last_name);
-    })   
-    }
-        var json={};
-        Post.find({
-                "_id": {"$in":ids}
-            })
-            .then(result=>{
-                console.log("result.length ::",result.length);
-                for(var i=0;i<result.length;i++)
-                {        
-                        var name=first_name[i]+" "+last_name[i];
-                        json["title"]=result[i].title;
-                        json["contents"]=result[i].contents;
-                        json["likes"]=result[i].like;
-                        json["comments"]=result[i].comment;
-                        json["created_at"]=result[i].created_at;
-                        // json["first_name"]=first_name[i];
-                        // json["last_name"]=last_name[i];
-                        json["name"]=name;
-                        arraylist.push(json);
-                 }
-            res.send({result:"success",message:"Data found successfully",data:arraylist});
-            });
-        }
-    } 
+} 
