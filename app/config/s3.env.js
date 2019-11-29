b8505a0727c077ae80036e00312778baedf1c278
GuruprasadADJ <57342055+GuruprasadADJ@users.noreply.github.com>
2019-11-29 17:35:30 +0530

    Update s3.env.js

--- app/config/s3.env.js
+++ app/config/s3.env.js
@@ -1,6 +1,6 @@
 const env = {
-  AWS_ACCESS_KEY: 'AKIAIOC7EM6OWWMUDF7A', // change to yours
-  AWS_SECRET_ACCESS_KEY: 'AVJRIzbItTNuP8iHRO7JuxLivIBOduIBGwlH7mhH', // change to yours
+  AWS_ACCESS_KEY: '', // change to yours
+  AWS_SECRET_ACCESS_KEY: '', // change to yours
   REGION : 'ap-south-1', // change to yours
   Bucket: 'sindhbookimages' // change to yours
   };
