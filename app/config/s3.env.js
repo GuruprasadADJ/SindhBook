3ac797733efff5149bf190a6f239b1c33e7ea2a4
GuruprasadADJ <57342055+GuruprasadADJ@users.noreply.github.com>
2019-11-30 11:12:57 +0530

    Delete s3.env.js

--- app/config/s3.env.js
+++
@@ -1,7 +0,0 @@
-const env = {
-  AWS_ACCESS_KEY: 'AKIAIOC7EM6OWWMUDF7A', // change to yours
-  AWS_SECRET_ACCESS_KEY: 'AVJRIzbItTNuP8iHRO7JuxLivIBOduIBGwlH7mhH', // change to yours
-  REGION : 'ap-south-1', // change to yours
-  Bucket: 'sindhbookimages' // change to yours
-  };
-  module.exports = env;
