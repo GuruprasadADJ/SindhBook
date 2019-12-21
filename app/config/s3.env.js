629f6c23dc7106655c3170484e09ffef2f9973f5
Guruprasad <guruprasad@adjinfotech.com>
2019-12-21 18:14:56 +0530

    dfdsd

--- app/config/s3.env.js
+++
@@ -1,9 +0,0 @@
-const env = {
-  AWS_ACCESS_KEY: 'AKIAIOS7EZTWW5SK7LRA', // change to yours
-  AWS_SECRET_ACCESS_KEY: 'lIIlQTlwhnkADDGZv12vTYEIwuGYK8eBF9jaxoTE', // change to yours
-  REGION : 'ap-south-1', // change to yours
-  Bucket: 'sindh-book' // change to yours
-  };
-  module.exports = env;
-  
-//security keyfile path :-  C:\Users\india\Downloads\rootkey(1)
